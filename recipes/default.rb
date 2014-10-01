b9ed5b0fe067e80704237fba7146575ef463ac1e
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-10-01 17:37:16 +0530

    moved all git pulling above

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,12 +8,12 @@ composer_project '/home/ubuntu/apps/core' do
 	action :update
 end
 
-execute 'making core storage writable' do
-	command 'sudo chmod -R a+w /home/ubuntu/apps/core/app/storage'
-end
-
 git '/home/ubuntu/apps/core/app/config/production' do
 	repository 'gitlab@gitlab.owlgrin.com:horntell/configs.git'
 	revision 'core'
 	action :sync
+end
+
+execute 'making core storage writable' do
+	command 'sudo chmod -R a+w /home/ubuntu/apps/core/app/storage'
 end
