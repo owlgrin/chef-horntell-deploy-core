0393931e1c7274c71af0c290f90db9ba826158cb
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-12-24 19:57:07 +0530

    added author email

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'deploy-core'
 maintainer       'Mohit Mamoria'
-maintainer_email 'YOUR_EMAIL'
+maintainer_email 'mohit.mamoria@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures deploy-core'
 long_description 'Installs/Configures deploy-core'
